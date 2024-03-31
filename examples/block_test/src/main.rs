a54c8b226248bbfde086a40a535b5cf9a0605716
Janne Kauppinen <janne.kauppinen4321@gmail.com>
2024-03-31 11:58:52 +0300

    Start working with xml parsing.

--- examples/block_test/src/main.rs
+++ examples/block_test/src/main.rs
@@ -1,11 +1,18 @@
+use quick_xml::name::QName;
 use json::misc::{
     load_from_file,
-    SourceIndexer,
+    seek_block,
+    seek_xml_block
 };
 
 pub fn main() {
     let source = load_from_file(String::from("joo.txt")).unwrap(); 
-    let indexer = SourceIndexer::init();
-    let indices = indexer.seek_block(&source, '{', '}');
+    // let indexer = SourceIndexer::init();
+    let indices = seek_block(&source, '{', '}');
     println!("indices :: {:?}", indices);
+
+    let source_xml = load_from_file(String::from("punamusta_tuho.dek")).unwrap(); 
+    // let source_xml = load_from_file(String::from("puna_kakka.dek")).unwrap(); 
+    let tags = vec![(QName(b"deck"), 0)];
+    seek_xml_block(&source_xml, &tags);
 }
--- json/src/misc.rs
+++ json/src/misc.rs
@@ -5,9 +5,15 @@ use std::error::Error;
 use std::path::Path;
 // use std::{error::Error, fmt::Debug};
 use thiserror::Error;
+use quick_xml::{
+    Reader,
+};
+use quick_xml::events::Event;
+use quick_xml::name::QName;
+
 
 #[derive(Error, Debug)]
-pub enum SourceIndexerError {
+pub enum BlockError {
     #[error("Malformed input. There are more `{0}` characters than `{1}` characters")]
     EndCharError(char, char),
 
@@ -16,57 +22,77 @@ pub enum SourceIndexerError {
 
 }
 
-pub struct SourceIndexer {
+pub fn seek_block(input_str: &String, start: char, end: char) -> Result<Vec<(u64, u64)>, BlockError> {
 
-    start_index: u32,
-    end_index: u32,
-    // temp_buffer: Vec[u8],
-}
-
-impl SourceIndexer {
-
-    pub fn init() -> Self {
-        Self {
-            start_index: 0,
-            end_index: 0,
+    let mut start_index = 0; 
+    let mut end_index = 0; 
+    let mut depth: i32 = 0;
+    let mut result = Vec::<(u64,u64)>::new();
+    
+    for c in input_str.char_indices() {
+        // The start of a block.
+        if c.1 == start && depth == 0 { // && start_index == end_index {
+            start_index = c.0; 
+            depth += 1;
+        }
+        else if c.1 == start {
+            depth += 1;
+        }
+        else if c.1 == end {
+            if depth == 0 { return Err(BlockError::EndCharError( start, end)); }
+            depth -= 1;
+            // An enclosing char found.
+            if depth == 0 {
+                end_index = c.0;
+                result.push(
+                    (start_index.try_into().unwrap(),
+                     end_index.try_into().unwrap())
+                );
+            }
         }
     }
 
-    pub fn seek_block(&self, input_str: &String, start: char, end: char) -> Result<Vec<(u64, u64)>, SourceIndexerError> {
+    if depth != 0 {
+        return Err(BlockError::OddNumberError(end, start)); 
+    }
 
-        let mut start_index = 0; 
-        let mut end_index = 0; 
-        let mut depth: i32 = 0;
-        let mut result = Vec::<(u64,u64)>::new();
-        
-        for c in input_str.char_indices() {
-            // The start of a block.
-            if c.1 == start && depth == 0 { // && start_index == end_index {
-                start_index = c.0; 
-                depth += 1;
-            }
-            else if c.1 == start {
-                depth += 1;
-            }
-            else if c.1 == end {
-                if depth == 0 { return Err(SourceIndexerError::EndCharError( start, end)); }
-                depth -= 1;
-                // An enclosing char found.
-                if depth == 0 {
-                    end_index = c.0;
-                    result.push(
-                        (start_index.try_into().unwrap(),
-                         end_index.try_into().unwrap())
-                    );
+    Ok(result)
+}
+
+pub fn seek_xml_blocks(input_str: &String, tags: &Vec<(QName, u32)>) {
+    let mut reader = Reader::from_str(input_str);
+    reader.trim_text(true);
+
+    let mut buffer = Vec::<u8>::new();
+    let mut buffer_position = reader.buffer_position();
+    loop {
+        match reader.read_event_into(&mut buffer).unwrap() {
+            Event::Start(e) => {
+                for x in tags {
+                    if e.name().as_ref() == x.0.local_name().as_ref() {
+
+                        let a = buffer_position;
+                        let mut span = reader.read_to_end(x.0).unwrap();
+                        buffer_position = reader.buffer_position();
+                        println!("{:?}", input_str[a..buffer_position].to_string());
+                        break;
+                    }
                 }
+            },
+            Event::End(e) => {
+                // buffer_position = reader.buffer_position();
+            }
+            Event::Empty(e) => {
+                let a = buffer_position;
+                buffer_position = reader.buffer_position();
+                println!("{:?}", input_str[a..buffer_position].to_string());
+            }
+            Event::Eof => { break; },
+            _ => {
+                   buffer_position = reader.buffer_position();
+                   continue;
             }
         }
-
-        if depth != 0 {
-            return Err(SourceIndexerError::OddNumberError(end, start)); 
-        }
-
-        Ok(result)
     }
 }
 
